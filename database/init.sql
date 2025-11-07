86b50572e8aed6191c3da4dba200b5862d23e3be
mtzl19 <mathegabr@gmail.com>
2025-11-07 19:04:13 -0300

    Criação da tabela de cosméticos

---
+++ database/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS cosmetics (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    type VARCHAR(50),
+    rarity VARCHAR(50),
+    image_url TEXT,
+    price INT DEFAULT 0, -- Preço em V-Bucks
+    is_new BOOLEAN DEFAULT FALSE,
+    is_for_sale BOOLEAN DEFAULT FALSE,
+    added_at TIMESTAMP -- Data de adição do item
+);
